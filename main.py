b5e3c2b34f8ed8f26cacfff6c11bad02dc29e4d6
ecke <jarekecke@aol.de>
2021-11-02 15:53:38 +0100

    Basic-Structure (New models should be implemented by creating new .py files and called by main() )

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from Dataloader import load
+#Project from Ines and Jarek
+from Dataloader import loader
+from RNNModel import rnn
 
+def main():
 
+    data_list = loader()
+    rnn(data_list)
+
+    return
 
 if __name__ == '__main__':
 
-    load()
-    #test
-    #testjarek-today-branch-jarek-123
+    main()
+
+
 
