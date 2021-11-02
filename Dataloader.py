779761124d8f49f3373f817c9a905f3c77eaefea
Ines-Maria <inesgrossweege@googlemail.com>
2021-11-02 15:29:44 +0100

    test

--- Dataloader.py
+++ Dataloader.py
@@ -1,5 +1,8 @@
 # Load the data
+import pandas as pd
 
 def load():
-    ...
-    return
+    df = pd.read_csv('C:/Users/inesg/Documents/Uni/Master/Seminar/breath_data/190114_peep_tit_1.csv')
+    #print(df)
+    #print(df[df.columns[::16]])
+    return(df[df.columns[::16]])
