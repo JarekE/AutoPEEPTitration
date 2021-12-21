45735cfb1c55ec4bb619a5d19325147e40e06367
ecke <jarekecke@aol.de>
2021-12-21 14:37:32 +0100

    100% Netz

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 # Define configs here
 
-#System
+# System
 
 model = "SimpleNN" #RNN oder SimpleNN oder Philip...
 
-#User
+# User
 
 # how many breathes per peep step (10,20,40)
 length = 20
-# only usable for the SimpleNN in the moment
+# number of epochs (only usable for the SimpleNN in the moment)
 epoch_number = 10000
 
 # calculate the gradient and use it
 grad = True
 
-#print
-print = False
+# print
+print = True
